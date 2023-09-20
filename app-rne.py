db3e61cc2857c01c43a285e294922d32e8254db7
kevinMPO <63031175+kevinMPO@users.noreply.github.com>
2023-09-20 16:14:59 +0200

    Update app-rne.py

--- app-rne.py
+++ app-rne.py
@@ -3,8 +3,7 @@ import requests
 import pandas as pd
 
 # Fonctions pour obtenir le token et les documents
-
-def get_token():
+def get_token(stage=1):
     url = "https://registre-national-entreprises.inpi.fr/api/sso/login"
     payload = {
         "username": "kmameri@scores-decisions.com",
@@ -14,10 +13,24 @@ def get_token():
     if response.status_code == 200:
         return response.json().get("token")
     else:
-        st.write(f"Erreur : {response.status_code}")  # Ajoutez cette ligne pour imprimer le code d'erreur
-        st.write(f"Réponse : {response.text}")  # Ajoutez cette ligne pour imprimer le texte de la réponse
+        st.write(f"Erreur : {response.status_code}")
+        st.write(f"Réponse : {response.text}")
         return None
-        
+
+@st.cache(allow_output_mutation=True)
+def get_documents(siren, token):
+    url = f"https://registre-national-entreprises.inpi.fr/api/companies/{siren}/attachments"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get("actes")
+    else:
+        st.write(f"Erreur lors de la récupération des documents : {response.status_code}")
+        st.write(f"Réponse : {response.text}")
+        return None
+
 # Configuration Streamlit
 st.set_page_config(
     page_title="Consultation des Actes d'Entreprises",
@@ -35,8 +48,8 @@ st.caption("App développée par Kevin MAMERI")
 # Entrée du SIREN
 siren = st.text_input("Veuillez entrer le SIREN de l'entreprise:")
 
-# Obtenir le token
-token = get_token()
+# Obtenir le token (étape 1)
+token = get_token(stage=1)
 
 if token and siren:
     documents = get_documents(siren, token)
@@ -54,8 +67,11 @@ if token and siren:
             doc_id = doc.get('id')
             download_button = None
             if doc_id:
-                # Création d'un lien de téléchargement qui appellera une fonction pour télécharger le document lorsqu'il sera cliqué
-                download_button = f'<a href="https://actes-pdf-rne.streamlit.app/download?doc_id={doc_id}&token={token}" target="_blank">Télécharger le document</a>'
+                # Obtenir un nouveau token pour l'étape 2
+                token_stage2 = get_token(stage=2)
+                if token_stage2:
+                    # Création d'un lien de téléchargement qui appellera une fonction pour télécharger le document lorsqu'il sera cliqué
+                    download_button = f'<a href="https://actes-pdf-rne.streamlit.app/download?doc_id={doc_id}&token={token_stage2}" target="_blank">Télécharger le document</a>'
                 
             data_list.append({
                 "Date de dépôt": date_depot,
